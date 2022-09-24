b45fa60bfcc837bfa06dba5239623c380a0a34ae
pablo-ramm <99685910+pablo-ramm@users.noreply.github.com>
2022-09-24 16:35:36 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const ejsMate = require('ejs-mate');
 const port = 3000
 const bodyParser = require('body-parser');
 const { localsName } = require('ejs');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+
+
+const dbUrl = "mongodb://localhost:27017/agricultureTest";
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    //useFindAndModify:false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 
 
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 }));
 
 app.engine('ejs', ejsMate) //to use a layout file 
@@ -22,8 +41,19 @@ app.get('/', (req, res) => {
 
 app.get('/home', (req, res) => {
     res.render('landing')
-  })
-  
+})
+
+app.get('/newProduct', (req, res) => {
+    res.render('newProduct')
+})
+
+app.post('/products', async (req, res) => {
+    console.log(req.body.product)
+    const product = new Product(req.body.product);
+    await product.save();
+    res.redirect('/')
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
